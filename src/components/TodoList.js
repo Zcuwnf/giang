8623c2b6e67b2db112adadc96874c5d1bdbce441
Phạm Giang <84945471+Zcuwnf@users.noreply.github.com>
2022-04-13 14:06:56 +0700

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,62 +1,43 @@
-// import React, { Component } from 'react'
-// import Todo from './Todo'
-
-// class TodoList extends Component {
-//     // const { listTodos, 
-//     //         checkAll,
-//     //         isCheckedAll 
-//     //     } = props
-//     render(){
-//         return (
-//         <section className="main">
-//             <input
-//                 className="toggle-all"
-//                 type="checkbox"
-//                 onChange={() => {
-//                     console.log('mmark done')
-//                     this.props.checkAll()
-//                 }}
-//                 checked={this.props.isCheckedAll}
-//             />
-//             <label htmlFor="toggle-all" onClick={this.props.checkAll} ></label>
-//             <ul className="todo-list">
-//                 {
-//                     this.props.listTodos.map((todo, index) => <Todo index={index} key={todo.id} todo={todo}  />)
-//                 }
-//             </ul>
-
-//         </section>
-//         )
-//     }
-// }
-
-// export default TodoList
-
-import React, { memo } from 'react'
+import React, { Component } from 'react'
 import Todo from './Todo'
 
-const TodoList = memo(props => {
-    const { listTodos, checkAll, isCheckedAll } = props
-    return (
+class TodoList extends Component {
+    // const { listTodos, 
+    //         checkAll,
+    //         isCheckedAll 
+    //     } = props
+    render(){
+        return (
         <section className="main">
             <input
                 className="toggle-all"
                 type="checkbox"
                 onChange={() => {
                     console.log('mmark done')
-                    checkAll()
+                    this.props.checkAll()
                 }}
-                checked={isCheckedAll}
+                checked={this.props.isCheckedAll}
             />
-            <label htmlFor="toggle-all" onClick={checkAll} ></label>
+            <label htmlFor="toggle-all" onClick={this.props.checkAll} ></label>
             <ul className="todo-list">
                 {
-                    listTodos.map((todo, index) => <Todo index={index} key={todo.id} todo={todo} {...props} />)
+                    this.props.listTodos.map((todo, index) => 
+                    <Todo index={index} 
+                        key={todo.id} 
+                        todo={todo}   
+                        markCompleted={this.props.markCompleted}
+                        todoEditingId={this.props.todoEditingId}
+                        getEditTodo={this.props.getEditTodo}
+                        editTodo={this.props.editTodo}
+                        removeTodo={this.props.removeTodo}
+                        
+                    />)
                 }
             </ul>
 
         </section>
-    )
-})
+        )
+    }
+}
 
 export default TodoList
