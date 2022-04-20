81bffff12d2a06385d5447ad5cdbf21037b34e5b
Phạm Giang <84945471+Zcuwnf@users.noreply.github.com>
2022-04-20 14:18:13 +0700

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,43 +1,72 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import Todo from './Todo'
+
+// class TodoList extends Component {
+//     // const { listTodos, 
+//     //         checkAll,
+//     //         isCheckedAll 
+//     //     } = props
+//     render(){
+//         return (
+//         <section className="main">
+//             <input
+//                 className="toggle-all"
+//                 type="checkbox"
+//                 onChange={() => {
+//                     console.log('mmark done')
+//                     this.props.checkAll()
+//                 }}
+//                 checked={this.props.isCheckedAll}
+//             />
+//             <label htmlFor="toggle-all" onClick={this.props.checkAll} ></label>
+//             <ul className="todo-list">
+//                 {
+//                     this.props.listTodos.map((todo, index) => 
+//                     <Todo index={index} 
+//                         key={todo.id} 
+//                         todo={todo}   
+//                         markCompleted={this.props.markCompleted}
+//                         todoEditingId={this.props.todoEditingId}
+//                         getEditTodo={this.props.getEditTodo}
+//                         editTodo={this.props.editTodo}
+//                         removeTodo={this.props.removeTodo}
+                        
+//                     />)
+//                 }
+//             </ul>
+
+//         </section>
+//         )
+//     }
+// }
+
+// export default TodoList
+
+import React, {  } from 'react'
 import Todo from './Todo'
 
-class TodoList extends Component {
-    // const { listTodos, 
-    //         checkAll,
-    //         isCheckedAll 
-    //     } = props
-    render(){
-        return (
+const TodoList = (props => {
+    const { listTodos, checkAll, isCheckedAll } = props
+    return (
         <section className="main">
             <input
                 className="toggle-all"
                 type="checkbox"
                 onChange={() => {
                     console.log('mmark done')
-                    this.props.checkAll()
+                    checkAll()
                 }}
-                checked={this.props.isCheckedAll}
+                checked={isCheckedAll}
             />
-            <label htmlFor="toggle-all" onClick={this.props.checkAll} ></label>
+            <label htmlFor="toggle-all" onClick={checkAll} ></label>
             <ul className="todo-list">
                 {
-                    this.props.listTodos.map((todo, index) => 
-                    <Todo index={index} 
-                        key={todo.id} 
-                        todo={todo}   
-                        markCompleted={this.props.markCompleted}
-                        todoEditingId={this.props.todoEditingId}
-                        getEditTodo={this.props.getEditTodo}
-                        editTodo={this.props.editTodo}
-                        removeTodo={this.props.removeTodo}
-                        
-                    />)
+                    listTodos.map((todo, index) => <Todo index={index} key={todo.id} todo={todo} {...props} />)
                 }
             </ul>
 
         </section>
-        )
-    }
-}
+    )
+})
 
 export default TodoList
