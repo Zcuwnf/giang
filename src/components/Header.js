041a1df76fe8dfc2477d7ec91660cc8e0aeeb7d0
Phạm Giang <84945471+Zcuwnf@users.noreply.github.com>
2022-04-20 14:15:47 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,69 +1,102 @@
-import React, { Component } from 'react'
-// import { render } from 'react-dom'
+// import React, { Component } from 'react'
+// // import { render } from 'react-dom'
+// import {v4 as uuidv4} from 'uuid'
+
+// class Header  extends Component {
+//     // // const [text, setText] = useState('')
+//     // const { addTodo } = props
+
+//     constructor(props) {
+//         super(props);
+//         this.state  ={
+//             text : "",
+//         }
+//     }
+
+//     setText = (e) =>{
+//       this.setState({
+//           [e.target.title]: e.target.value,
+//       })  
+//     }
+
+//     // onAddTodos = (event) => {
+//     //     if (event.key === 'Enter' && text) {
+//     //         addTodo({
+//     //             id: new Date().valueOf(),
+//     //             text,
+//     //             isCompleted: false
+//     //         })
+//     //         setText('')
+//     //     }
+//     // }
+
+//     onAddTodos = (event) => {
+//       event.preventDefault();
+//       this.props.addTodo({
+//         id: uuidv4(),
+//         text: this.state.text,
+//         isCompleted: false,
+//       });
+//       this.setState({
+//         text: "",
+//       });
+//     };
+
+//   render()
+//     {  return (
+//         <header className="header">
+//             <h1>Todo List</h1>
+//             <input
+//                 className="new-todo"
+//                 placeholder="Enter your todo"
+//                 //value={text}
+//                 value={this.state.text}
+//                 title="text"
+//                 // onChange={(e) => setText(e.target.value)}
+//                 onChange={this.setText}
+//                 // onKeyPress={(e) => onAddTodo(e)}
+//                 onKeyDown={(event) =>{
+//                   if (event.key === 'Enter'){
+//                     this.onAddTodos(event)
+//                   }
+//                 }}
+//             />
+//         </header>
+//     );
+//   }
+// }
+
+// export default Header
+
+import React, {useState} from "react"
 import {v4 as uuidv4} from 'uuid'
 
-class Header  extends Component {
-    // // const [text, setText] = useState('')
-    // const { addTodo } = props
-
-    constructor(props) {
-        super(props);
-        this.state  ={
-            text : "",
+const Header = (props =>{
+    const [text, setText] = useState('')
+    const {addTodo} = props
+    const onAddTodo = (e = {} ) => {
+        if (e.key === 'Enter' && text) {
+        
+            addTodo({
+                id: uuidv4(),
+                text,
+                checked: false
+            })
+            setText('')
         }
     }
-
-    setText = (e) =>{
-      this.setState({
-          [e.target.title]: e.target.value,
-      })  
-    }
-
-    // onAddTodos = (event) => {
-    //     if (event.key === 'Enter' && text) {
-    //         addTodo({
-    //             id: new Date().valueOf(),
-    //             text,
-    //             isCompleted: false
-    //         })
-    //         setText('')
-    //     }
-    // }
-
-    onAddTodos = (event) => {
-      event.preventDefault();
-      this.props.addTodo({
-        id: uuidv4(),
-        text: this.state.text,
-        isCompleted: false,
-      });
-      this.setState({
-        text: "",
-      });
-    };
-
-  render()
-    {  return (
+    return (
         <header className="header">
             <h1>Todo List</h1>
-            <input
-                className="new-todo"
+            <input 
+                className="new-todo" 
                 placeholder="Enter your todo"
-                //value={text}
-                value={this.state.text}
-                title="text"
-                // onChange={(e) => setText(e.target.value)}
-                onChange={this.setText}
-                // onKeyPress={(e) => onAddTodo(e)}
-                onKeyDown={(event) =>{
-                  if (event.key === 'Enter'){
-                    this.onAddTodos(event)
-                  }
-                }}
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                onKeyDown={(e) => onAddTodo(e)}
             />
         </header>
-    );
-  }
-}
+    )
+})
 
 export default Header
