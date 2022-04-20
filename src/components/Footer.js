71b54b0968f3ea8f31a1c59a14f88a0bd2226079
Phạm Giang <84945471+Zcuwnf@users.noreply.github.com>
2022-04-20 14:19:23 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,76 @@
-import React, { Component } from 'react'
-// import { render } from 'react-dom'
+// import React, { Component } from 'react'
+// // import { render } from 'react-dom'
 
-class Footer extends Component {
-    // const { setStatusFilter, activeButton, clearCompleted, numOfTodosLeft, numOfTodos } = props
-    render(){
-        return (
+// class Footer extends Component {
+//     // const { setStatusFilter, activeButton, clearCompleted, numOfTodosLeft, numOfTodos } = props
+//     render(){
+//         return (
+//         <footer className="footer">
+//             <span className="todo-count">
+//                 <strong>{this.props.numOfTodosLeft}</strong>
+//                 <span> </span>
+//                 <span>{this.props.numOfTodosLeft > 1 ? 'items' : 'item'}</span>
+//                 <span> left</span>
+//             </span>
+//             <ul className="filters">
+//                 <li>
+//                     <a
+//                         href="#/"
+//                         className={`${this.props.activeButton === 'ALL' ? "selected" : ''}`}
+//                         onClick={() => this.props.setStatusFilter('ALL')}
+//                     >
+//                         All
+//                     </a>
+//                 </li>
+//                 <span></span>
+//                 <li>
+//                     <a
+//                         href="#/active"
+//                         className={`${this.props.activeButton === 'ACTIVE' ? "selected" : ''}`}
+//                         onClick={() => this.props.setStatusFilter('ACTIVE')}
+//                     >
+//                         Active
+//                     </a>
+//                 </li>
+//                 <span></span>
+//                 <li>
+//                     <a
+//                         href="#/completed"
+//                         className={`${this.props.activeButton === 'COMPLETED' ? "selected" : ''}`}
+//                         onClick={() => this.props.setStatusFilter('COMPLETED')}
+//                     >
+//                         Completed
+//                     </a>
+//                 </li>
+//             </ul>
+//             {
+//                 this.props.numOfTodosLeft < this.props.numOfTodos && <button className="clear-completed" onClick={this.props.clearCompleted}>Clear completed</button>
+//             }
+//         </footer>
+//         )
+//     }
+// }
+
+// export default Footer
+
+import React, { } from 'react'
+
+const Footer = ((props) => {
+    const { setStatusFilter, activeButton, clearCompleted, numOfTodosLeft, numOfTodos } = props
+    return (
         <footer className="footer">
             <span className="todo-count">
-                <strong>{this.props.numOfTodosLeft}</strong>
+                <strong>{numOfTodosLeft}</strong>
                 <span> </span>
-                <span>{this.props.numOfTodosLeft > 1 ? 'items' : 'item'}</span>
+                <span>{numOfTodosLeft > 1 ? 'items' : 'item'}</span>
                 <span> left</span>
             </span>
             <ul className="filters">
                 <li>
                     <a
                         href="#/"
-                        className={`${this.props.activeButton === 'ALL' ? "selected" : ''}`}
-                        onClick={() => this.props.setStatusFilter('ALL')}
+                        className={`${activeButton === 'ALL' ? "selected" : ''}`}
+                        onClick={() => setStatusFilter('ALL')}
                     >
                         All
                     </a>
@@ -26,8 +79,8 @@ class Footer extends Component {
                 <li>
                     <a
                         href="#/active"
-                        className={`${this.props.activeButton === 'ACTIVE' ? "selected" : ''}`}
-                        onClick={() => this.props.setStatusFilter('ACTIVE')}
+                        className={`${activeButton === 'ACTIVE' ? "selected" : ''}`}
+                        onClick={() => setStatusFilter('ACTIVE')}
                     >
                         Active
                     </a>
@@ -36,19 +89,18 @@ class Footer extends Component {
                 <li>
                     <a
                         href="#/completed"
-                        className={`${this.props.activeButton === 'COMPLETED' ? "selected" : ''}`}
-                        onClick={() => this.props.setStatusFilter('COMPLETED')}
+                        className={`${activeButton === 'COMPLETED' ? "selected" : ''}`}
+                        onClick={() => setStatusFilter('COMPLETED')}
                     >
                         Completed
                     </a>
                 </li>
             </ul>
             {
-                this.props.numOfTodosLeft < this.props.numOfTodos && <button className="clear-completed" onClick={this.props.clearCompleted}>Clear completed</button>
+                numOfTodosLeft < numOfTodos && <button className="clear-completed" onClick={clearCompleted}>Clear completed</button>
             }
         </footer>
-        )
-    }
-}
+    )
+})
 
 export default Footer
